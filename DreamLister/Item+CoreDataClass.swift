a9c34554e3ab3e209d66b821aed9ff30445810a8
Othmar Gispert <ogispert@Othmars-MacBook-Pro.local>
2017-05-18 19:07:16 -0500

    First Commit

---
+++ DreamLister/Item+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Item+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by Integra Connect on 10/26/16.
+//  Copyright © 2016 Personal. All rights reserved.
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(Item)
+public class Item: NSManagedObject {
+
+}
---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,26 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Integra Connect on 10/26/16.
+//  Copyright © 2016 Personal. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+    func configureCell(item: Item) {
+        
+        title.text = item.title
+        price.text = "$\(item.price)"
+        details.text = item.details
+        thumb.image = item.toImage?.image as? UIImage
+    }
+    
+}
